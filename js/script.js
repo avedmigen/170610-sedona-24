ab174d74194b33562d8c072ea5dac1e18f4e9f63
Dmitry Averin <avedmigen@gmail.com>
2019-03-12 20:07:20 +0300

    Исправляет доступность формы на главной без js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var searchbtn = document.querySelector(".search-hotel__button");
 var searchform = document.querySelector(".search-hotel-form");
 
+searchform.classList.add("visually-hidden");
+
 searchbtn.addEventListener("click", function (event) {
     event.preventDefault();
     searchform.classList.toggle("visually-hidden");
